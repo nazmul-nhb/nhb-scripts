c572af5fdb7829d84a3ea78a07af07c72928fc9c
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-07-25 02:59:35 +0600

    update(module template): updated mongoose module template

--- templates/express-mongoose-zod.mjs
+++ templates/express-mongoose-zod.mjs
@@ -5,7 +5,8 @@ import { capitalizeString, pluralizer } from 'nhb-toolbox';
 /** @type {import('../types/index.d.ts').expressMongooseZodTemplate} */
 export function expressMongooseZodTemplate(moduleName) {
 	const capModule = capitalizeString(moduleName);
-	const pluralModule = pluralizer.toPlural(capModule);
+	const pluralModule = pluralizer.toPlural(moduleName);
+	const pluralCapModule = pluralizer.toPlural(capModule);
 
 	return [
 		{
@@ -24,7 +25,17 @@ router.post(
 	${moduleName}Controllers.create${capModule},
 );
 
-router.get('/', ${moduleName}Controllers.getAll${pluralModule});
+router.get('/', ${moduleName}Controllers.getAll${pluralCapModule});
+
+router.get('/:id', ${moduleName}Controllers.getSingle${capModule});
+
+router.patch(
+	'/:id',
+	validateRequest(${moduleName}Validations.updateSchema),
+	${moduleName}Controllers.update${capModule},
+);
+
+router.delete('/:id', ${moduleName}Controllers.delete${capModule});
 
 export const ${moduleName}Routes = router;
             `,
@@ -42,45 +53,116 @@ const create${capModule} = catchAsync(async (req, res) => {
 	sendResponse(res, '${capModule}', 'POST', new${capModule});
 });
             
-const getAll${pluralModule} = catchAsync(async (_req, res) => {
-    const ${moduleName}s = await ${moduleName}Services.getAll${pluralModule}FromDB();
+const getAll${pluralCapModule} = catchAsync(async (_req, res) => {
+    const ${pluralModule} = await ${moduleName}Services.getAll${pluralCapModule}FromDB();
 
-    sendResponse(res, '${capModule}', 'GET', ${moduleName}s);
+    sendResponse(res, '${capModule}', 'GET', ${pluralModule});
 });
 
-export const ${moduleName}Controllers = { getAll${pluralModule} };
+const getSingle${capModule} = catchAsync(async (req, res) => {
+	const ${moduleName} = await ${moduleName}Services.getSingle${capModule}FromDB(req?.params?.id);
+
+	sendResponse(res, '${capModule}', 'GET', ${moduleName});
+});
+
+const update${capModule} = catchAsync(async (req, res) => {
+	const ${moduleName} = await ${moduleName}Services.update${capModule}InDB(req?.params?.id, req?.body);
+
+	sendResponse(res, '${capModule}', 'PATCH', ${moduleName});
+});
+
+const delete${capModule} = catchAsync(async (req, res) => {
+	await ${moduleName}Services.delete${capModule}FromDB(req?.params?.id);
+
+	sendResponse(res, '${capModule}', 'DELETE');
+});
+
+export const ${moduleName}Controllers = {
+	create${capModule},
+	getAll${pluralCapModule},
+	getSingle${capModule},
+	update${capModule},
+	delete${capModule},
+};
             `,
 		},
 		{
 			name: `${moduleName}.services.ts`,
 			content: `
+import { ErrorWithStatus } from '../../classes/ErrorWithStatus';
 import { QueryBuilder } from '../../classes/QueryBuilder';
+import { STATUS_CODES } from '../../constants/index';
 import { ${capModule} } from './${moduleName}.model';
 import type { I${capModule} } from './${moduleName}.types';
 
 const create${capModule}InDB = async (payload: I${capModule}) => {
-	const new${moduleName} = await ${capModule}.create(payload);
+	const new${capModule} = await ${capModule}.create(payload);
 
-	return new${moduleName};
+	return new${capModule};
 };
 
-const getAll${pluralModule}FromDB = async (query?: Record<string, unknown>) => {
+const getAll${pluralCapModule}FromDB = async (query?: Record<string, unknown>) => {
     const ${moduleName}Query = new QueryBuilder(${capModule}.find(), query).sort();
-    // const ${moduleName}s = await ${capModule}.find({});
+    // const ${pluralModule} = await ${capModule}.find({});
 
-    const ${moduleName}s = await ${moduleName}Query.modelQuery;
+    const ${pluralModule} = await ${moduleName}Query.modelQuery;
 
-    return ${moduleName}s;
+    return ${pluralModule};
 };
 
-export const ${moduleName}Services = { create${capModule}InDB, getAll${pluralModule}FromDB };
+const getSingle${capModule}FromDB = async (id: string) => {
+	const ${moduleName} = await ${capModule}.find${capModule}ById(id);
+
+	return ${moduleName};
+};
+
+const update${capModule}InDB = async (id: string, payload: Partial<I${capModule}>) => {
+	const updated${capModule} = await ${capModule}.findOneAndUpdate({ _id: id }, payload, {
+		runValidators: true,
+		new: true,
+	});
+
+	if (!updated${capModule}) {
+		throw new ErrorWithStatus(
+			'Not Updated Error',
+			\`Cannot update specified ${moduleName} with ID \${ id }!\`,
+			STATUS_CODES.INTERNAL_SERVER_ERROR,
+			'update_${moduleName}',
+		);
+	}
+
+	return updated${capModule};
+};
+
+const delete${capModule}FromDB = async (id: string) => {
+	const result = await ${capModule}.deleteOne({ _id: id });
+
+	if (result.deletedCount < 1) {
+		throw new ErrorWithStatus(
+			'Delete Failed Error',
+			\`Failed to delete ${moduleName} with ID \${ id }!\`,
+			STATUS_CODES.INTERNAL_SERVER_ERROR,
+			'delete_${moduleName}',
+		);
+	}
+};
+
+export const ${moduleName}Services = {
+	create${capModule}InDB,
+	getAll${pluralCapModule}FromDB,
+	getSingle${capModule}FromDB,
+	update${capModule}InDB,
+	delete${capModule}FromDB,
+};
             `,
 		},
 		{
 			name: `${moduleName}.model.ts`,
 			content: `
 import { Schema, model } from 'mongoose';
-import type { I${capModule}Doc } from './${moduleName}.types';
+import type { I${capModule}Doc, I${capModule}Model } from './${moduleName}.types';
+import { ErrorWithStatus } from '../../classes/ErrorWithStatus';
+import { STATUS_CODES } from '../../constants/index';
 
 const ${moduleName}Schema = new Schema<I${capModule}Doc>(
     {
@@ -95,7 +177,31 @@ const ${moduleName}Schema = new Schema<I${capModule}Doc>(
 	},
 );
 
-export const ${capModule} = model<I${capModule}Doc>('${pluralModule}', ${moduleName}Schema);
+${moduleName}Schema.statics.find${capModule}ById = async function (id: string) {
+	if (!id) {
+		throw new ErrorWithStatus(
+			'Bad Request',
+			'Please provide a valid ID!',
+			STATUS_CODES.BAD_REQUEST,
+			'${moduleName}',
+		);
+	}
+
+	const ${moduleName} = await this.findById(id);
+
+	if (!${moduleName}) {
+		throw new ErrorWithStatus(
+			'Not Found Error',
+			\`No ${moduleName} found with ID \${ id }!\`,
+			STATUS_CODES.NOT_FOUND,
+			'${moduleName}',
+		);
+	}
+
+	return ${moduleName};
+};
+
+export const ${capModule} = model<I${capModule}Doc, I${capModule}Model>('${pluralCapModule}', ${moduleName}Schema);
             `,
 		},
 		{
@@ -107,13 +213,15 @@ const creationSchema = z
     .object({})
     .strict();
 
-export const ${moduleName}Validations = { creationSchema };
+const updateSchema = creationSchema.optional();
+
+export const ${moduleName}Validations = { creationSchema, updateSchema };
             `,
 		},
 		{
 			name: `${moduleName}.types.ts`,
 			content: `
-import type { Document, Types } from 'mongoose';
+import type { Document, Model, Types } from 'mongoose';
 
 export interface I${capModule} {
     // Define I${capModule} interface
@@ -124,6 +232,10 @@ export interface I${capModule}Doc extends I${capModule}, Document {
 	_id: Types.ObjectId;
 	created_at: string;
 	updated_at: string;
+}
+
+export interface I${capModule}Model extends Model<I${capModule}Doc> {
+	find${capModule}ById: (id: string) => Promise<I${capModule}Doc>;
 }
             `,
 		},
