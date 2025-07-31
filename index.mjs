c967852b2060a59770a6cda4b074db34c4c24ee1
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-08-01 01:05:44 +0600

    feat(helpers): added new helpers to be used with onComplete hook of 'nhb-module' script

--- index.mjs
+++ index.mjs
@@ -6,6 +6,8 @@ export { fixTypeExports } from './lib/fix-types-exports.mjs';
 
 export { fixJsExtensions, fixTsExtensions } from './lib/fix-imports.mjs';
 
+export { updateCollection, updateRoutes } from './lib/updateTemplate.mjs'
+
 export { runFormatter } from './lib/prettier-formatter.mjs';
 
 export { expressMongooseZodTemplate } from './templates/express-mongoose-zod.mjs';
