e6cfdc18f3ff7f88d72a302b8708350b6ad12e58
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-08-06 11:46:23 +0600

    test(husky): testing husky setup with lint-staged

--- bin/husky.mjs
+++ bin/husky.mjs
@@ -3,15 +3,48 @@
 
 // @ts-check
 
+import { intro, outro, spinner } from '@clack/prompts';
+import chalk from 'chalk';
+import { execa } from 'execa';
+
 import { installDeps } from '../lib/install-deps.mjs';
 import { parsePackageJson } from '../lib/package-json-utils.mjs';
+import path from 'path';
+import { existsSync } from 'fs';
+import { writeFile } from 'fs/promises';
+import { mimicClack } from '../lib/clack-utils.mjs';
 
 async function initHusky() {
+	intro(chalk.cyanBright('🐕 Setup Husky with Lint-Staged'));
+
 	const devDeps = ['husky', 'lint-staged'];
+
+	const s = spinner();
+
+	s.start(chalk.yellowBright("🔃 Installing 'husky' and 'lint-staged'"));
 
 	if (!isHuskyInstalled()) {
 		await installDeps([], devDeps);
 	}
+
+	s.stop(chalk.green("✅ Installed 'husky' and 'lint-staged'!"));
+
+	await execa('husky', ['init'], { cwd: process.cwd() });
+
+	const cwd = process.cwd();
+	const lsPath = path.join(cwd, '.lintstagedrc.json');
+	const huskyPath = path.join(cwd, '.husky/pre-commit');
+
+	await writeFile(huskyPath, `lint-staged`, 'utf-8');
+
+	const lsContent = JSON.stringify({ '*.+(js|ts)': ['prettier --write'] }, null, 2);
+
+	if (!existsSync(lsPath)) {
+		await writeFile(lsPath, lsContent, 'utf-8');
+		mimicClack(chalk.gray('⚙️  Created default ".lintstagedrc.json"'));
+	}
+
+	outro(chalk.cyanBright(`⚙️  Husky Setup Complete!`));
 }
 
 /**
@@ -26,11 +59,13 @@ function isHuskyInstalled() {
 			(pkg.dependencies &&
 				pkg.dependencies.husky &&
 				pkg.dependencies?.['lint-staged']) ||
-				(pkg.devDependencies &&
-					pkg.devDependencies.prettier &&
-					pkg.devDependencies?.['lint-staged'])
+			(pkg.devDependencies &&
+				pkg.devDependencies.husky &&
+				pkg.devDependencies?.['lint-staged'])
 		);
 	} catch {
-		return false;
+		return false
 	}
 }
+
+initHusky().catch(console.dir)
--- nhb.scripts.config.mjs
+++ nhb.scripts.config.mjs
@@ -1,19 +1,24 @@
 // @ts-check
 
-import { defineScriptConfig, expressMongooseZodTemplate, fixJsExtensions, fixTypeExports } from "./index.mjs";
+import {
+    defineScriptConfig,
+    expressMongooseZodTemplate,
+    fixJsExtensions,
+    fixTypeExports,
+} from './index.mjs';
 
 export default defineScriptConfig({
     format: {
         args: ['--write'],
-        files: ['lib', 'bin', 'templates', 'types'],
+        files: ['lib', 'bin', 'templates', 'types', 'nhb.scripts.config.mjs'],
         ignorePath: '.prettierignore',
     },
     commit: {
-        runFormatter: true,
+        runFormatter: false,
     },
     count: {
         defaultPath: 'lib',
-        excludePaths: ['node_modules', 'dist', 'build']
+        excludePaths: ['node_modules', 'dist', 'build'],
     },
     build: {
         distFolder: 'dist',
@@ -23,19 +28,18 @@ export default defineScriptConfig({
             {
                 cmd: 'tsc',
                 args: ['-p', 'tsconfig.esm.json'],
-                options: { stdio: 'inherit' }
-            }
+                options: { stdio: 'inherit' },
+            },
         ],
         after: [
             async () => await fixJsExtensions('dist/esm'),
-            async () => await fixTypeExports({
-                distPath: 'dist/dts',
-                packageJsonPath: 'package.json',
-                typeFileCandidates: ['types.d.ts', 'interfaces.d.ts'],
-                extraPatterns: [
-                    { pattern: 'plugins', folderName: 'plugins' },
-                ]
-            }),
+            async () =>
+                await fixTypeExports({
+                    distPath: 'dist/dts',
+                    packageJsonPath: 'package.json',
+                    typeFileCandidates: ['types.d.ts', 'interfaces.d.ts'],
+                    extraPatterns: [{ pattern: 'plugins', folderName: 'plugins' }],
+                }),
         ],
     },
     module: {
@@ -46,25 +50,31 @@ export default defineScriptConfig({
             'express-mongoose-zod': {
                 createFolder: true,
                 destination: 'src/app/modules',
-                files: expressMongooseZodTemplate // pre-built module : function that receives moduleName as argument and creates pre-defined files and contents
+                files: expressMongooseZodTemplate, // pre-built module : function that receives moduleName as argument and creates pre-defined files and contents
             },
             'my.template1': {
                 createFolder: true, // if `false` does not create folder with the module name from cli
                 destination: 'src/app', // optional, will prioritize inputs from cli
                 // Use dynamic moduleName in filenames and contents
                 files: (moduleName) => [
-                    { name: `${moduleName}.controllers.ts`, content: `// controllers for ${moduleName}` },
-                    { name: `${moduleName}.services.ts`, content: `// services for ${moduleName}` }
-                ]
+                    {
+                        name: `${moduleName}.controllers.ts`,
+                        content: `// controllers for ${moduleName}`,
+                    },
+                    {
+                        name: `${moduleName}.services.ts`,
+                        content: `// services for ${moduleName}`,
+                    },
+                ],
             },
-            'my_template2': {
+            my_template2: {
                 destination: 'src/features', // optional, will prioritize inputs from cli
                 // Use static file list with contents
                 files: [
                     { name: 'index.ts', content: '// content' },
-                    { name: 'dummy.js', content: '// dummy' }
-                ]
+                    { name: 'dummy.js', content: '// dummy' },
+                ],
             },
         },
-    }
+    },
 });
