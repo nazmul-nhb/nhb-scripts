a97142064af735ca70dd46363a4ea1d222871a4c
Nazmul Hassan <nazmulnhb007@yahoo.com>
2025-08-06 12:06:41 +0600

    fix(pre-commit): updated pre-commit header

--- nhb.scripts.config.mjs
+++ nhb.scripts.config.mjs
@@ -1,80 +1,80 @@
 // @ts-check
 
 import {
-    defineScriptConfig,
-    expressMongooseZodTemplate,
-    fixJsExtensions,
-    fixTypeExports,
+	defineScriptConfig,
+	expressMongooseZodTemplate,
+	fixJsExtensions,
+	fixTypeExports,
 } from './index.mjs';
 
 export default defineScriptConfig({
-    format: {
-        args: ['--write'],
-        files: ['lib', 'bin', 'templates', 'types', 'nhb.scripts.config.mjs'],
-        ignorePath: '.prettierignore',
-    },
-    commit: {
-        runFormatter: false,
-    },
-    count: {
-        defaultPath: 'lib',
-        excludePaths: ['node_modules', 'dist', 'build'],
-    },
-    build: {
-        distFolder: 'dist',
-        deleteDist: true,
-        commands: [
-            { cmd: 'tsc', args: ['-p', 'tsconfig.cjs.json'] },
-            {
-                cmd: 'tsc',
-                args: ['-p', 'tsconfig.esm.json'],
-                options: { stdio: 'inherit' },
-            },
-        ],
-        after: [
-            async () => await fixJsExtensions('dist/esm'),
-            async () =>
-                await fixTypeExports({
-                    distPath: 'dist/dts',
-                    packageJsonPath: 'package.json',
-                    typeFileCandidates: ['types.d.ts', 'interfaces.d.ts'],
-                    extraPatterns: [{ pattern: 'plugins', folderName: 'plugins' }],
-                }),
-        ],
-    },
-    module: {
-        destination: 'src/modules', // optional, default: "src/modules"
-        defaultTemplate: 'my.template1', // selected by default, must match with the keys of `templates` object
-        force: false, // `true` if you want to override the existing module
-        templates: {
-            'express-mongoose-zod': {
-                createFolder: true,
-                destination: 'src/app/modules',
-                files: expressMongooseZodTemplate, // pre-built module : function that receives moduleName as argument and creates pre-defined files and contents
-            },
-            'my.template1': {
-                createFolder: true, // if `false` does not create folder with the module name from cli
-                destination: 'src/app', // optional, will prioritize inputs from cli
-                // Use dynamic moduleName in filenames and contents
-                files: (moduleName) => [
-                    {
-                        name: `${moduleName}.controllers.ts`,
-                        content: `// controllers for ${moduleName}`,
-                    },
-                    {
-                        name: `${moduleName}.services.ts`,
-                        content: `// services for ${moduleName}`,
-                    },
-                ],
-            },
-            my_template2: {
-                destination: 'src/features', // optional, will prioritize inputs from cli
-                // Use static file list with contents
-                files: [
-                    { name: 'index.ts', content: '// content' },
-                    { name: 'dummy.js', content: '// dummy' },
-                ],
-            },
-        },
-    },
+	format: {
+		args: ['--write'],
+		files: ['lib', 'bin', 'templates', 'types', 'nhb.scripts.config.mjs'],
+		ignorePath: '.prettierignore',
+	},
+	commit: {
+		runFormatter: false,
+	},
+	count: {
+		defaultPath: 'lib',
+		excludePaths: ['node_modules', 'dist', 'build'],
+	},
+	build: {
+		distFolder: 'dist',
+		deleteDist: true,
+		commands: [
+			{ cmd: 'tsc', args: ['-p', 'tsconfig.cjs.json'] },
+			{
+				cmd: 'tsc',
+				args: ['-p', 'tsconfig.esm.json'],
+				options: { stdio: 'inherit' },
+			},
+		],
+		after: [
+			async () => await fixJsExtensions('dist/esm'),
+			async () =>
+				await fixTypeExports({
+					distPath: 'dist/dts',
+					packageJsonPath: 'package.json',
+					typeFileCandidates: ['types.d.ts', 'interfaces.d.ts'],
+					extraPatterns: [{ pattern: 'plugins', folderName: 'plugins' }],
+				}),
+		],
+	},
+	module: {
+		destination: 'src/modules', // optional, default: "src/modules"
+		defaultTemplate: 'my.template1', // selected by default, must match with the keys of `templates` object
+		force: false, // `true` if you want to override the existing module
+		templates: {
+			'express-mongoose-zod': {
+				createFolder: true,
+				destination: 'src/app/modules',
+				files: expressMongooseZodTemplate, // pre-built module : function that receives moduleName as argument and creates pre-defined files and contents
+			},
+			'my.template1': {
+				createFolder: true, // if `false` does not create folder with the module name from cli
+				destination: 'src/app', // optional, will prioritize inputs from cli
+				// Use dynamic moduleName in filenames and contents
+				files: (moduleName) => [
+					{
+						name: `${moduleName}.controllers.ts`,
+						content: `// controllers for ${moduleName}`,
+					},
+					{
+						name: `${moduleName}.services.ts`,
+						content: `// services for ${moduleName}`,
+					},
+				],
+			},
+			my_template2: {
+				destination: 'src/features', // optional, will prioritize inputs from cli
+				// Use static file list with contents
+				files: [
+					{ name: 'index.ts', content: '// content' },
+					{ name: 'dummy.js', content: '// dummy' },
+				],
+			},
+		},
+	},
 });
